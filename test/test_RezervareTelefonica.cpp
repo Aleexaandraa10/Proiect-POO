72b53521e6e429a5e7d9d56533e1ca770f239034
Aleexaandraa10 <147261523+Aleexaandraa10@users.noreply.github.com>
2024-04-20 16:07:11 +0300

    Update test_RezervareTelefonica.cpp

--- test/test_RezervareTelefonica.cpp
+++ test/test_RezervareTelefonica.cpp
@@ -24,22 +24,7 @@ TEST(RezervareTelefonicaTest, OperatorEqual) {
     EXPECT_FALSE(rezervare1 == rezervare3);
 }
 
-// Test afisare
-TEST(RezervareTelefonicaTest, Afisare) {
-    RezervareTelefonica rezervare(4, "12:00", "05.05.2023", "Etaj", "Bianca", "1234567890");
-    testing::internal::CaptureStdout();
-    rezervare.afisare(std::cout);
-    std::string output = testing::internal::GetCapturedStdout();
-
-    std::string expected_output = "Numele pe care a fost facuta rezervarea este: Bianca\n";
-    expected_output += "Nr de persoane care participa este: 4\n";
-    expected_output += "Ora la care masa a fost rezervata este: 12:00\n";
-    expected_output += "Data rezervarii este: 05.05.2023\n";
-    expected_output += "Zona restaurantului aleasa este: Etaj\n";
-    expected_output += "Iar: 1234567890 este nr. de telefon de pe care a fost facuta rezervarea\n";
-    EXPECT_EQ(output, expected_output);
-}
-
+//Test operator = si Getters
 TEST(RezervareTelefonicaTest, OperatorAtribuire) {
     RezervareTelefonica rezervare1(4, "12:00", "02.05.2024", "Parter", "Mihai", "0775232042");
     RezervareTelefonica rezervare2;
