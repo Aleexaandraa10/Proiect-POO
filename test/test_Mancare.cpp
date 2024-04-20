3a3d84a07bd3a7a7f9d397d20a0e9d348109e6ed
Aleexaandraa10 <147261523+Aleexaandraa10@users.noreply.github.com>
2024-04-20 16:03:31 +0300

    Update test_Mancare.cpp

--- test/test_Mancare.cpp
+++ test/test_Mancare.cpp
@@ -1,8 +1,8 @@
 #include "gtest/gtest.h"
 #include "Mancare.h"
 
-//Teste la Getters
-TEST(MancareSuiteName, MancareTestName){
+//Testare la Getters + constructor
+TEST(MancareTest, MancareTestName){
     Mancare mancare("Penne italiano", "Paste",300,45);
     EXPECT_EQ(mancare.GetPret(),45);
     EXPECT_EQ(mancare.GetGramaj(),300);
