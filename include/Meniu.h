fbd2db50cb0f09cf0591ba36e7ef6c42f8187162
Aleexaandraa10 <147261523+Aleexaandraa10@users.noreply.github.com>
2024-05-18 12:39:27 +0300

    Update Meniu.h

--- include/Meniu.h
+++ include/Meniu.h
@@ -1,8 +1,20 @@
 #ifndef MENIU_H
 #define MENIU_H
 
-#include "Restaurant.h"
-#include "Burger.h"
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include "Rezervare.h"
+#include "RezervareOnline.h"
+#include "RezervareTelefonica.h"
+#include "RezervareAbonament.h"
+#include "Mancare.h"
+#include "Exceptii.h"
+
+#include "Evaluare.h"
+#include "EvaluareMancare.h"
+#include "EvaluareAtmosfera.h"
+#include "EvaluareServire.h"
 
 class Meniu{
 private:
@@ -12,6 +24,7 @@ public:
     Meniu& operator=(const Meniu&) = delete;
 
     static Meniu& get_meniu();
+    static void ControlPanel();
 };
 
 #endif //MENIU_H
