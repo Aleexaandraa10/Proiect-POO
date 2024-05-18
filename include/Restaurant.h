248ac651d614978e61cfe8ccf198ced20f9a13e1
Aleexaandraa10 <147261523+Aleexaandraa10@users.noreply.github.com>
2024-05-18 12:39:49 +0300

    Update Restaurant.h

--- include/Restaurant.h
+++ include/Restaurant.h
@@ -4,29 +4,26 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
-#include "Rezervare.h"
-#include "RezervareOnline.h"
-#include "RezervareTelefonica.h"
-#include "Mancare.h"
-#include "Exceptii.h"
+#include "Meniu.h"
 
 class Restaurant {
 private:
     std::vector<Rezervare*> Detalii_Rezervari;
     std::vector<Mancare> Detalii_Preparate;
-    static int count;
 
 public:
     Restaurant();
     ~Restaurant();
     void Creare_Rezervare();
     void Modificare_Detalii_Rezervare();
-    void OreDisponibile();
-    void Recomandari_Culinare();
+    void OreDisponibile() const;
+    void Recomandari_Culinare() const;
     void Numara_Rezervari(const std::string& ora) const;
     void Anuleaza_Rezervare() const;
     static void aruncaExceptie();
     static void prindeExceptie();
+    void Evaluare_Clienti(std::string& nume, std::string& data);
+    void Medie_Evaluari() const;
 };
 
 #endif // RESTAURANT_H
